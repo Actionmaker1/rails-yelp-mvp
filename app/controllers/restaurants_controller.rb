63d8ed6a3fb37e609b97d18335df57cbf66e2b27
Alexander von Croy <alexander@voncroy.eu>
2020-11-11 16:28:04 +0100

    yelp

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,30 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    def create
+      restaurant = Restaurant.new(restaurant_params)
+      restaurant.save
+      redirect_to restaurants_path
+    end
+  end
+
+  private
+ 
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating)
+  end
+end
